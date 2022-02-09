ee2a77e034a7cb066b672e0533aa36401b316d8a
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-02-09 09:04:31 -0400

    Created VS project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_PassBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pass By Value vs. Pass By Reference");
+            Console.WriteLine("(related to passing perams to methods)");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("");
+
+            int a = 100;
+            Console.WriteLine(a);
+            Test(ref a);
+            Console.WriteLine(a);
+
+            //Test (ref a);
+
+
+
+
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+
+        //static void test(ref int a)
+        static void Test(ref int a)
+        {
+            a = 200;
+        }
+
+        //pass by ref - default for classes
+        //pass by value - default for vars
+    }
+}
